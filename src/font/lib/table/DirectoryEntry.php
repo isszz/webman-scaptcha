4a8a60da0b851f3d50fe1da41d7161fdf03abdcc
isszz <isszz@qq.com>
2026-08-30 00:59:33 +0800

    Refactor checksum calculation in DirectoryEntry.php
    Replaced manual checksum calculation with array_sum on unpacked data.

--- src/font/lib/table/DirectoryEntry.php
+++ src/font/lib/table/DirectoryEntry.php
@@ -46,20 +46,8 @@ class DirectoryEntry extends BinaryStream
 			$data = str_pad($data, $len + (4 - $mod), "\0");
 		}
 
-		$len = strlen($data);
-
-		$hi = 0x0000;
-		$lo = 0x0000;
-
-		for ($i = 0; $i < $len; $i += 4) {
-			$hi += (ord($data[$i]) << 8) + ord($data[$i + 1]);
-			$lo += (ord($data[$i + 2]) << 8) + ord($data[$i + 3]);
-			$hi += $lo >> 16;
-			$lo = $lo & 0xFFFF;
-			$hi = $hi & 0xFFFF;
-		}
-
-		return ($hi << 8) + $lo;
+	    $table = unpack("N*", $data);
+	    return array_sum($table);
 	}
 
 	public function __construct(File $font)
